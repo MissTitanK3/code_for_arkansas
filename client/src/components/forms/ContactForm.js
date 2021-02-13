12f404c6fe96a9e0a25e09e30144888e53881ce7
MissTitanK3 <tianna.m.mccoy@gmail.com>
2021-02-13 13:51:07 -0600

    Updated contact form. And links for social icons

--- client/src/components/forms/ContactForm.js
+++ client/src/components/forms/ContactForm.js
@@ -2,12 +2,31 @@ import React from 'react'
 
 export default function ContactForm() {
   return (
-    <div>
+    <div className='contactForm'>
       <form action="submit">
-        <label>
-          <p>Name: </p>
-        </label>
-        <input type="text" name="name" id="name" placeholder='Full Name' />
+        <h3>Have a question?</h3>
+        <div className='flexing'>
+          <div>
+            <label>
+              <p>Name: </p>
+            </label>
+            <input type="text" name="name" id="name" placeholder='Full Name' />
+            <label>
+              <p>Email: </p>
+            </label>
+            <input type="email" name="email" id="email" placeholder='Email' />
+            <label>
+              <p>Subject: </p>
+            </label>
+            <input type="text" name="subject" id="subject" placeholder='Subject' />
+          </div>
+          <div>
+            <label>
+              <p>Message: </p>
+            </label>
+            <textarea name="text" id="text" cols="30" rows="10"></textarea>
+          </div>
+        </div>
       </form>
     </div>
   )
--- client/src/components/nav/NavFooter.js
+++ client/src/components/nav/NavFooter.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import ContactForm from '../forms/ContactForm'
+import * as FaIcons from "react-icons/fa";
+import { Link } from "react-router-dom";
 import './Nav.css'
 
 export default function NavFooter() {
   return (
     <div className='footer'>
       <p>Sitemap</p>
-      <p>Social Links</p>
+      <ul>
+        <li>
+          <Link>Meetup&nbsp;&nbsp;&nbsp;<FaIcons.FaMeetup size='2em' /></Link>
+        </li>
+        <li>
+          <Link>Slack&nbsp;&nbsp;&nbsp;<FaIcons.FaSlack size='2em' /></Link>
+        </li>
+        <li>
+          <Link>Email&nbsp;&nbsp;&nbsp;<FaIcons.FaPaperPlane size='2em' /></Link>
+        </li>
+      </ul>
       <ContactForm />
     </div>
   )
